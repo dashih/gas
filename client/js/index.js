f8b9514b6713b0b3582a7b18a68f917fc61b6f40
Danny Shih <danny@dannyshih.net>
2022-05-22 21:24:15 -0700

    Documents sha256 code.

--- client/js/index.js
+++ client/js/index.js
@@ -5,11 +5,14 @@ const Status = Object.freeze({
     'Error': 3
 });
 
-async function sha256(password) {
-    const passwordEncoded = new TextEncoder().encode(password);
-    const hashBuffer =  await crypto.subtle.digest('SHA-256', passwordEncoded);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+/*
+ * https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+ */
+async function digestMessage(message) {
+    const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
     return hashHex;
 }
 
@@ -178,7 +181,7 @@ document.getElementById('submitButton').onclick = async () => {
     // Generate nonce and append to password.
     const nonce = self.crypto.randomUUID();
     const passwordPlusNonce = password + '.' + nonce;
-    const passwordHash = await sha256(passwordPlusNonce);
+    const passwordHash = await digestMessage(passwordPlusNonce);
     const payload = {
         'passwordHash': passwordHash,
         'nonce': nonce,
